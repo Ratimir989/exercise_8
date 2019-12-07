58af79bf01eae02b2f59da90fd8c216771094447
Ratimir <ra0953029364@gmail.com>
2019-12-07 17:30:50 +0200

    Добавьте файлы проекта.

---
+++ exercise_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сделайте свою ставку");
+            int rate = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            int value = rnd.Next(1, 12);
+            Console.WriteLine("Бросаем кубик");
+            Console.WriteLine($"Выпало значение, {value}");
+            if (value <=5)
+            {
+                Console.WriteLine("вы проиграли");
+            }
+            else if (value >5 && value <= 8)
+            {
+                Console.WriteLine($"Вы выиграли, {rate}");
+            }
+            else if (value > 8 && value <=11)
+            {
+                Console.WriteLine($"Вы выиграли, {rate*2}");
+            }
+            else
+            {
+                Console.WriteLine($"Вы выиграли, {rate * 10}");
+            }
+
+        }
+    }
+}
